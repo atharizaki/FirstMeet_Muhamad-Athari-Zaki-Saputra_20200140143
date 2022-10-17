614c3bdfec11a28f32efc1070a5126686bf2283b
atharizaki <atharizaki4@gmail.com>
2022-10-17 23:08:12 +0700

    Import Apache

---
+++ src/main/java/PABD_C/Database_TokoObat/DatabaseTokoObatApplication.java
@@ -0,0 +1,13 @@
+package PABD_C.Database_TokoObat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTokoObatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseTokoObatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PABD_C/Database_TokoObat/DatabaseTokoObatApplicationTests.java
@@ -0,0 +1,13 @@
+package PABD_C.Database_TokoObat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseTokoObatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
