404fc7bf5c7acd451782b1ff4b23d9a30729d387
atharizaki <atharizaki4@gmail.com>
2022-10-17 23:22:26 +0700

    Add JPA controller

---
+++ src/main/java/PABD_C/Database_TokoObat/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package PABD_C.Database_TokoObat.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
